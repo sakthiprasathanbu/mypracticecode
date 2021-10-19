d97f10f2693d4af1b2979b6975701ac101e1ad89
sakthiprasath anbu <71253743+sakthiprasathanbu@users.noreply.github.com>
2021-10-19 16:10:20 +0530

    Add files via upload

---
+++ coinToss.py
@@ -0,0 +1,30 @@
+import random 
+
+toss = random.randint(0,1)
+if toss == 1:
+    print("""
+
+    88        88 88888888888        db        88888888ba,    
+    88        88 88                d88b       88       `8b   
+    88        88 88               d8'`8b      88        `8b  
+    88aaaaaaaa88 88aaaaa         d8'  `8b     88         88  
+    88aaaaaaaa88 88aaaaa        d8YaaaaY8b    88         88  
+    88        88 88            d8pppppppp8b   88         8P  
+    88        88 88           d8'        `8b  88      .a8P   
+    88        88 88888888888 d8'          `8b 88888888Y'    
+        
+    
+    """) 
+else:
+    print("""
+
+   888888888888   db        88 88          ad88888ba   
+        88       d88b       88 88         d8`     `8b  
+        88      d8'`8b      88 88         Y8,          
+        88     d8'  `8b     88 88         `Y8aaaaa,    
+        88    d8YaaaaY8b    88 88           ``````8b,  
+        88   d8````````8b   88 88                 `8b  
+        88  d8'        `8b  88 88         Y8a     a8P  
+        88 d8'          `8b 88 88888888888 `Y88888P`  
+                                                        
+    """)
